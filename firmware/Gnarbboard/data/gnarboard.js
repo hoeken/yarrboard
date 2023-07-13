a6ae57a14cdc0ad4b529f678f76db28aed6bd6bd
Zach Hoeken <hoeken@gmail.com>
2023-07-12 23:56:18 -0500

    okay bootstrapification is coming along nicely.

--- firmware/Gnarbboard/data/gnarboard.js
+++ firmware/Gnarbboard/data/gnarboard.js
@@ -1,77 +1,80 @@
+var socket = new WebSocket("ws://" + window.location.host + "/ws");
 
-let socket = new WebSocket("ws://" + window.location.host + "/ws");
+function start_gnarboard() {
+    socket.onopen = function(e) {
+        console.log("[open] Connection established");
+    };
+      
+      socket.onmessage = function(event) {
+        const msg = JSON.parse(event.data);
+        //console.log(msg);
+      
+        if (msg.msg == 'config')
+        {
+          $('#nameHeader').html(msg.name);
+          $('#statsContainer').html(`Uptime:${msg.uptime}<br/>Messages: ${msg.totalMessages}<br/>UUID: ${msg.uuid}`);
+      
+          $('#channelTableBody').html("");
+          for (ch of msg.channels)
+          {
+            $('#channelTableBody').append(`<tr id="channel${ch.id}" class="channelRow"></tr>`);
+            $('#channel' + ch.id).append(`<td class="text-center"><button id="channelState${ch.id}" type="button" class="btn btn-sm" onclick="toggle_state(${ch.id})"></button></td>`);
+            $('#channel' + ch.id).append(`<td id="channelName${ch.id}" class="channelName">${ch.name}</td>`);
+            $('#channel' + ch.id).append(`<td id="channelDutyCycle${ch.id}" class="text-end"></td>`);
+            $('#channel' + ch.id).append(`<td id="channelCurrent${ch.id}" class="text-end"></td>`);
+            //$('#channel' + ch.id).append(`<td id="channelAmpHours${ch.id}" class="text-end"></td>`);
+            $('#channel' + ch.id).append(`<td id="channelError${ch.id}" class="channelError"></td>`);
+          }
+        }
+        
+        if (msg.msg == 'update')
+        {
+          $('#time').html(msg.time);
+          for (ch of msg.channels)
+          {
+            if (ch.state)
+            {
+              $('#channelState' + ch.id).html("ON");
+              $('#channelState' + ch.id).removeClass("btn-secondary");
+              $('#channelState' + ch.id).addClass("btn-success");
+            }
+            else
+            {
+              $('#channelState' + ch.id).html("OFF");
+              $('#channelState' + ch.id).removeClass("btn-success");
+              $('#channelState' + ch.id).addClass("btn-secondary");
+            }
+      
+            let duty = Math.round(ch.duty * 100);
+            //let current = Math.round(ch.current * 100) / 100;
+            let current = ch.current.toFixed(2);
+            let aH = Math.round(ch.aH * 100) / 100;
+            $('#channelDutyCycle' + ch.id).html(`${duty}%`);
+            $('#channelCurrent' + ch.id).html(`${current}A`);
+            //$('#channelAmpHours' + ch.id).html(`${aH}aH`);
+      
+            if (msg.soft_fuse_tripped)
+              $('#channelError' + ch.id).html("Soft Fuse Tripped");
+          }
+        }
+      };
+      
+      socket.onclose = function(event) {
+        if (event.wasClean) {
+          console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+        } else {
+          // e.g. server process killed or network down
+          // event.code is usually 1006 in this case
+          console.log('[close] Connection died');
+        }
+      };
+      
+      socket.onerror = function(error) {
+        console.log(`[error]`);
+      };
+      
+}
 
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-};
-
-socket.onmessage = function(event) {
-  const msg = JSON.parse(event.data);
-  //console.log(msg);
-
-  if (msg.msg == 'config')
-  {
-    $('#nameHeader').html(msg.name);
-    $('#stats').html(`Uptime:${msg.uptime}<br/>Messages: ${msg.totalMessages}<br/>UUID: ${msg.uuid}`);
-
-    $('#channelTable').html("<tr><th>State</th><th>Name</th><th>Duty</th><th>Current</th></tr>");
-    for (ch of msg.channels)
-    {
-      $('#channelTable').append(`<tr id="channel${ch.id}" class="channelRow"></tr>`);
-      $('#channel' + ch.id).append(`<td><button id="channelState${ch.id}" class="channelState" onclick="toggle_state(${ch.id})"></button></td>`);
-      $('#channel' + ch.id).append(`<td id="channelName${ch.id}" class="channelName">${ch.name}</td>`);
-      $('#channel' + ch.id).append(`<td id="channelDutyCycle${ch.id}" class="channelDutyCycle"></td>`);
-      $('#channel' + ch.id).append(`<td id="channelCurrent${ch.id}" class="channelCurrent"></td>`);
-      //$('#channel' + ch.id).append(`<td id="channelAmpHours${ch.id}" class="channelAmpHours"></td>`);
-      $('#channel' + ch.id).append(`<td id="channelError${ch.id}" class="channelError"></td>`);
-    }
-  }
-  
-  if (msg.msg == 'update')
-  {
-    $('#time').html(msg.time);
-    for (ch of msg.channels)
-    {
-      if (ch.state)
-      {
-        $('#channelState' + ch.id).html("ON");
-        $('#channelState' + ch.id).removeClass("buttonOff");
-        $('#channelState' + ch.id).addClass("buttonOn");
-      }
-      else
-      {
-        $('#channelState' + ch.id).html("OFF");
-        $('#channelState' + ch.id).removeClass("buttonOn");
-        $('#channelState' + ch.id).addClass("buttonOff");
-      }
-
-      let duty = Math.round(ch.duty * 100);
-      //let current = Math.round(ch.current * 100) / 100;
-      let current = ch.current.toFixed(2);
-      let aH = Math.round(ch.aH * 100) / 100;
-      $('#channelDutyCycle' + ch.id).html(`${duty}%`);
-      $('#channelCurrent' + ch.id).html(`${current}A`);
-      //$('#channelAmpHours' + ch.id).html(`${aH}aH`);
-
-      if (msg.soft_fuse_tripped)
-        $('#channelError' + ch.id).html("Soft Fuse Tripped");
-    }
-  }
-};
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.log(`[error]`);
-};
 
 function toggle_state(id)
 {
