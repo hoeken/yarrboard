73675931db07a1068fb06358d6e69d0bb1bac675
Zach Hoeken <hoeken@gmail.com>
2023-08-01 13:28:32 -0500

    quick bump to check ota

--- firmware/make_release.sh
+++ firmware/make_release.sh
@@ -6,16 +6,19 @@ echo "Making release $1"
 #cat .pio/build/esp32dev/firmware.sign .pio/build/esp32dev/firmware.bin > .pio/build/esp32dev/signed.bin
 #cp ".pio/build/esp32dev/signed.bin" "releases/yarrboard-${1}.bin"
 
+#copy our files over
 cp ".pio/build/esp32dev/firmware.bin" "releases/yarrboard-${1}.bin"
 cp ".pio/build/esp32dev/spiffs.bin" "releases/spiffs-${1}.bin"
 
+#version bump
 sed -i "s|[0-9]*\.[0-9]*\.[0-9]*|$1|g" firmware.json
 
+#checklist
 echo ""
 echo "Did you remember to?"
 echo ""
-echo "Build fresh firmware"
-echo "Build fresh filesystem image"
 echo "Bump the version # in platformio.ini"
 echo "Bump the version # in index.html"
+echo "Build fresh firmware"
+echo "Build fresh filesystem image"
 echo "Add your changelist to firmware.json"
